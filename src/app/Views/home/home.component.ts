69a64fac95e4f29ad652c2d6b5b563f68d740005
imad eddine ZAOUI <imads@imads-MacBook-Pro.local>
2023-12-28 22:40:27 +0100

    Add footer component

--- src/app/Views/home/home.component.ts
+++ src/app/Views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../Components/footer/footer.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [FooterComponent]
 })
 export class HomeComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Views/home/home.component';
 
 export const routes: Routes = [
-    // { path:'admin/dashboard', component : AdminDashboardComponent,}
+     { path:'home', component : HomeComponent,}
     
 ];
